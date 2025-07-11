855639826fb4f2f57c5476bfc6d5326f42a86dd3
Ayush Shinde <96916808+Ayush09006@users.noreply.github.com>
2025-07-11 23:42:20 +0530

    Delete pd_backend directory

--- pd_backend/core/consumers.py
+++
@@ -1,7 +0,0 @@
-from channels.generic.websocket import AsyncWebsocketConsumer
-import json
-
-class PoseConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        await self.accept()
-        await self.send(text_data=json.dumps({"msg": "WebSocket Connected"}))
--- pd_backend/core/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from .consumers import PoseConsumer
-
-websocket_urlpatterns = [
-    re_path(r'ws/pose/$', PoseConsumer.as_asgi()),
-]
